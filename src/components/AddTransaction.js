3bc1604b2d9ed7dcc84d6c4b7fda0e5e2f1104a3
ashispavan <ashispavan@live.com>
2020-05-24 16:44:26 -0400

    Added custom expense labels

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -5,8 +5,8 @@ function AddTransaction() {
 
 	const { addTransaction } = useContext(GlobalContext);
 
-	const [text, setText] = useState('');
-	const [amount, setAmount] = useState(0);
+	const [text, setText] = useState('Longos');
+	const [amount, setAmount] = useState('');
 
 	const onSubmit = (e) => {
 		e.preventDefault();
@@ -18,8 +18,8 @@ function AddTransaction() {
 		}
 		
 		addTransaction(newTransaction);
-		setText('');
-		setAmount(0);
+		setText('Longos');
+		setAmount('');
 
 	}
 
@@ -28,8 +28,22 @@ function AddTransaction() {
 		<h3>Add new transaction</h3>
 		<form onSubmit={onSubmit}>
 			<div className="form-control">
-				<label htmlFor="text">Text</label>
-				<input type="text" onChange={(e) => setText(e.target.value)}  value={text} id="text" placeholder="Enter text..." />
+				{/* <label htmlFor="text">Text</label> */}
+				{/* <input type="text" onChange={(e) => setText(e.target.value)}  value={text} id="text" placeholder="Enter text..." /> */}
+				<select value={text} onChange={(e) => setText(e.target.value)}>
+					<option value="Longos">Longos</option>
+					<option value="Hydro">Hydro</option>
+					<option value="Dollarama">Dollarama</option>
+					<option value="Rogers">Rogers</option>
+					<option value="FineIndiaGroceries">Fine India Groceries</option>
+					<option value="Eggsmart">Eggsmart</option>
+					<option value="UberEats">Uber Eats</option>
+					<option value="IndianCrown">Indian Crown</option>
+					<option value="Insurance">Insurance</option>
+					<option value="Rent">Rent</option>
+					<option value="Miscellaneous">Miscellaneous</option>
+					Select
+          		</select>
 			</div>
 			<div className="form-control">
 				<label htmlFor="amount"
